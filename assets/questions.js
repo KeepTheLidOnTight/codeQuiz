d8c3f7950d97f7da44b8eaf278bfdb4f210569c5
Ryan Freese <ryan.p.freese@gamil.com>
2020-10-01 09:07:10 -0500

    Worked into the night to secure functionality of at least the questions of the quiz, along with time deduction for incorrect answers.

---
+++ assets/questions.js
@@ -0,0 +1,30 @@
+//array of questions regarding Ben & Jerry's
+console.log(answers)
+var questions = [
+    {   //this is the first question and answers
+        question: "What all is in Half Baked?",
+        answers: ["Chocolate & Vanilla Ice Creams mixed with Gobs of Chocolate Chip Cookie Dough & Fudge Brownies", "Chocolate Ice Cream with White & Dark Fudge Chunks, Pecans, Walnuts & Fudge-Covered Almonds", "Vanilla Ice Cream with Chocolate Sandwich Cookies & Chocolate Cookie Swirls", "Vanilla Malt Ice Cream with Peanutty Fudge-Covered Pretzels with Fudge & Peanut Buttery Swirls"],
+        correct: 0
+    },
+    {   //this is the second question and answers
+        question: "What all is in AmeriCone Dream?",
+        answers: ["Chocolate Ice Cream with Fudge Brownies", "Coffee Ice Cream with Fudge Chunks & a Brownie Batter Swirl", "Vanilla Ice Cream with Fudge-Covered Waffle Cone Pieces & a Caramel Swirl", "Cold Brew Coffee Ice Cream with Chocolate Cookie-Covered Coffee Liqueur Truffles & Fudge Swirls"],
+        correct: 2   
+    },
+    {   //this is the third question and answers
+        question: "What all is in Cherry Garcia?",
+        answers: ["Coffee Ice Cream with Fudge Chunks & a Brownie Batter Swirl", "Cherry Ice Cream with Cherries & Fudge Flakes", "Vanilla Ice Cream with Gobs of Chocolate Chip Cookie Dough", "Caramel Cheesecake Ice Cream with Cheesecake Pieces & Chocolate Cookie Swirls"],
+        correct: 1       
+    },
+    {   //this is the fourth question and answers
+        question: "What all is in Chunky Monkey?",
+        answers: ["Caramel Ice Cream with Cinnamon Bun Dough & a Cinnamon Streusel Swirl", "Chocolate Malt Milkshake Ice Cream With Salted Fudge Pieces & Marshmallow Swirls", "Chocolate Ice Cream with Chocolate Cookies & Swirls of Chocolate Pudding Ice Cream", "Banana Ice Cream with Fudge Chunks & Walnuts"],
+        correct: 3 
+    },
+    {  //this is the fifth & final question and answers
+        question: "What all is in Phish Food?",
+        answers: ["Pumpkin Cheesecake Ice Cream with a Graham Cracker Swirl", "Coffee Ice Cream with Fudge Chunks & a Brownie Batter Swirl", "Chocolate Ice Cream with Gooey Marshmallow Swirls, Caramel Swirls & Fudge Fish", "Milk Chocolate Ice Cream with Peanut Buttery Swirls & Chocolate Cookie Swirls"],
+        correct: 2
+    }
+];
+console.log(answers)
--- assets/script.js
+++ assets/script.js
@@ -8,35 +8,8 @@ var questionAnswer = document.getElementById("questionAnswer");
 var submitScore = document.getElementById("submitScore");
 var currentTime = 100;
 var intervalTimer;
-var currentQuestionI = 0
-//array of questions regarding Ben & Jerry's
-var questions = [
-    {   //this is the first question and answers
-        question1: "What all is in Half Baked?",
-        answers: ["A: Chocolate & Vanilla Ice Creams mixed with Gobs of Chocolate Chip Cookie Dough & Fudge Brownies", "Chocolate Ice Cream with White & Dark Fudge Chunks, Pecans, Walnuts & Fudge-Covered Almonds", "Vanilla Ice Cream with Chocolate Sandwich Cookies & Chocolate Cookie Swirls", "Vanilla Malt Ice Cream with Peanutty Fudge-Covered Pretzels with Fudge & Peanut Buttery Swirls"],
-        correct: "A: Chocolate & Vanilla Ice Creams mixed with Gobs of Chocolate Chip Cookie Dough & Fudge Brownies",
-    },
-    {   //this is the second question and answers
-        question2: "What all is in AmeriCone Dream?",
-        answers: ["Chocolate Ice Cream with Fudge Brownies", "Coffee Ice Cream with Fudge Chunks & a Brownie Batter Swirl", "Vanilla Ice Cream with Fudge-Covered Waffle Cone Pieces & a Caramel Swirl", "Cold Brew Coffee Ice Cream with Chocolate Cookie-Covered Coffee Liqueur Truffles & Fudge Swirls"],
-        correct: "Vanilla Ice Cream with Fudge-Covered Waffle Cone Pieces & a Caramel Swirl"    
-    },
-    {   //this is the third question and answers
-        question3: "What all is in Cherry Garcia?",
-        answers: ["Coffee Ice Cream with Fudge Chunks & a Brownie Batter Swirl", "Cherry Ice Cream with Cherries & Fudge Flakes", "Vanilla Ice Cream with Gobs of Chocolate Chip Cookie Dough", "Caramel Cheesecake Ice Cream with Cheesecake Pieces & Chocolate Cookie Swirls"],
-        correct: "Cherry Ice Cream with Cherries & Fudge Flakes",       
-    },
-    {   //this is the fourth question and answers
-        question4: "What all is in Chunky Monkey?",
-        answers: ["Caramel Ice Cream with Cinnamon Bun Dough & a Cinnamon Streusel Swirl", "Chocolate Malt Milkshake Ice Cream With Salted Fudge Pieces & Marshmallow Swirls", "Chocolate Ice Cream with Chocolate Cookies & Swirls of Chocolate Pudding Ice Cream", "Banana Ice Cream with Fudge Chunks & Walnuts"],
-        correct: "Banana Ice Cream with Fudge Chunks & Walnuts", 
-    },
-    {  //this is the fifth & final question and answers
-        question5: "What all is in Phish Food?",
-        answers: ["Pumpkin Cheesecake Ice Cream with a Graham Cracker Swirl", "Coffee Ice Cream with Fudge Chunks & a Brownie Batter Swirl", "Chocolate Ice Cream with Gooey Marshmallow Swirls, Caramel Swirls & Fudge Fish", "Milk Chocolate Ice Cream with Peanut Buttery Swirls & Chocolate Cookie Swirls"],
-        correct: "Chocolate Ice Cream with Gooey Marshmallow Swirls, Caramel Swirls & Fudge Fish"
-    }
-]
+var currentQuestionIndex = 0;
+
 
 //hiding elements of html
 questionAnswer.style.display = "none"
@@ -55,10 +28,7 @@ function startTimer() {
         currentTime--
         if (currentTime <= 0) {
             window.clearInterval(intervalTimer)
-            alert("You ran out of time!")
-           
-            
-        }
+           }
         timer.textContent = "Time: " + currentTime + " seconds"
         
     }, 1000)
@@ -66,11 +36,45 @@ function startTimer() {
     
 }
 
-//displaying the questions and answers
+//displaying the questions and answers -- solution referenced but also follows my own code
 function pullQuestion() {
-   document.getElementById("questions").innerHTML = questions.question1
+    var orderedList = ["A:", "B:", "C:", "D:"]
+    console.log(answers)
+   var currentQuestion = questions[currentQuestionIndex];
+   var theQuestion = document.getElementById("questions");
+   theQuestion.textContent = currentQuestion.question;
+//    answers.innerHTML = "";
+   var listItems = document.getElementById("answers").querySelectorAll("li");
+   console.log(listItems)
+   listItems.forEach(function(element, i)  {
+       element.innerHTML = orderedList[i] + currentQuestion.answers[i]
+      element.addEventListener("click", checkAnswer(i))
+   });
+//    currentQuestion.answers.forEach(function() {
+       
+//    });
+   
 
     
+}
+function checkAnswer(index) {
+    return function(event) {
+        if (index === questions[currentQuestionIndex].correct) {
+            questions[currentQuestionIndex].correctAnswer = true;
+            currentQuestionIndex ++;
+            if (currentQuestionIndex >= questions.length) {
+                // sending user to high score page after quiz is finished
+                window.location.href = "highscores.html"
+                
+            } else {
+                pullQuestion()
+            }  
+        } else {
+           currentTime -= 10
+           questions[currentQuestionIndex].correctAnswer = false; 
+        }
+        
+    }
 }
     
 
